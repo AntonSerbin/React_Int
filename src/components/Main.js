91e99de367c8c3c494d6cc0d39e876083d539e98
Антон Сербин <antons.zn@gmail.com>
2018-12-17 17:47:32 +0200

    slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Slider from './Slider'
 
 const Repair = styled.div`
 color: #ffffff;
@@ -62,6 +63,7 @@ class Main extends Component{
 
 				</Col>
 				<Col lg={6} lgOffset={1}>
+					<Slider />
 
 				</Col>
 			</Row>
